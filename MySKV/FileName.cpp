1f590c93d6210a6305f386f3bd7bc7393bcd5eed
gkmnflz <gkmmflz323@gmail.com>
2025-12-28 23:44:23 +0200

    Added a variable and entered a formula for it.

--- MySKV/FileName.cpp
+++ MySKV/FileName.cpp
@@ -3,9 +3,11 @@
 #include <locale.h>
 void main(void) {
 	setlocale(LC_ALL, "");
-	float x;
+	float x, y;
 	printf("Введите x: ");
 	scanf_s("%f", &x);
-	printf("Вы ввели %.1f", x);
+	printf("Вы ввели %.1f\n", x);
+	y = x * x;
+	printf("y = %.1f", y);
 	_getch();
 }
